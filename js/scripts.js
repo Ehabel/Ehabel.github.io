b7bddc581171a85e83e6c23a31cdc7761d795386
Ehab El Cheikh <eelcheikh@hotmail.com>
2023-03-12 00:47:29 +1100

    Hamburger menu closes after clicking on item

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 function myFunction() {
     var x = document.querySelector(".header__content__nav-bar__menu");
+    var allLinks = document.getElementsByClassName("header__link");
     x.classList.toggle("--show");
+
+    for (var i = 0, n = allLinks.length; i < n; i++) {
+        allLinks[i].onclick = function () {
+            x.classList.toggle("--show");
+        };
+    }
 }
