7d22d78823b9432aca4de98332600415b637a166
Ehab El Cheikh <eelcheikh@hotmail.com>
2023-03-17 12:02:21 +1100

    Js revision

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function myFunction() {
-    var x = document.querySelector(".header__content__navbar__menu");
-    var allLinks = document.getElementsByClassName(
+    let x = document.querySelector(".header__content__navbar__menu");
+    let allLinks = document.getElementsByClassName(
         "header__content__navbar__menu__link"
     );
     x.classList.toggle("--show");
 
-    for (var i = 0, n = allLinks.length; i < n; i++) {
+    for (let i = 0, n = allLinks.length; i < n; i++) {
         allLinks[i].onclick = function () {
             x.classList.toggle("--show");
         };
